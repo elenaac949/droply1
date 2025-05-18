5bda7a5d651690f29c5d2fe95504dbbd0b23f4c0
Elena A.C. <161213022+elenaac949@users.noreply.github.com>
2025-05-18 21:36:49 +0200

    arreglo de problemas en el login

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -4,13 +4,20 @@ import { LoginComponent } from './components/auth/login/login.component';
 import { RegisterComponent } from './components/auth/register/register.component';
 import { HomeMapComponent } from './components/home-map/home-map.component';
 import { AuthGuard } from './guards/auth.guard';
+import { ProfileComponent } from './components/profile/profile.component';
+import { WaterFormComponent } from './components/water-form/water-form.component';
+import { AdminComponent } from './components/admin/admin.component';
+
 
 
 export const routes: Routes = [
     {path: '', component: LandingComponent}, //ruta predeterminada 
     {path: 'login', component: LoginComponent},
     {path: 'signup', component: RegisterComponent},
-    { path: 'map', component: HomeMapComponent, canActivate: [AuthGuard] },
+    {path: 'map', component: HomeMapComponent, canActivate: [AuthGuard] },
+    {path: 'profile', component: ProfileComponent, canActivate: [AuthGuard] },
+    {path: 'admin', component: AdminComponent, canActivate: [AuthGuard] },
+    {path: 'water-form', component: WaterFormComponent, canActivate: [AuthGuard] },
     {path: '**', redirectTo: ''} //las rutas que no se encuentran te llevan al landing
     
 ];
---
+++ frontend/src/app/components/water-form/water-form.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-water-form',
+  imports: [],
+  templateUrl: './water-form.component.html',
+  styleUrl: './water-form.component.css'
+})
+export class WaterFormComponent {
+
+}
