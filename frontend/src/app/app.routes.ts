d862b710e6baf35ca1152df336ce34734763fa7a
Elena A.C. <161213022+elenaac949@users.noreply.github.com>
2025-05-26 23:01:10 +0200

    dividir homemap en home y map

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -2,11 +2,12 @@ import { Routes } from '@angular/router';
 import { LandingComponent } from './components/landing/landing.component';
 import { LoginComponent } from './components/auth/login/login.component';
 import { RegisterComponent } from './components/auth/register/register.component';
-import { HomeMapComponent } from './components/home-map/home-map.component';
 import { AuthGuard } from './guards/auth.guard';
 import { ProfileComponent } from './components/profile/profile.component';
 import { WaterFormComponent } from './components/water-form/water-form.component';
 import { AdminComponent } from './components/admin/admin.component';
+import { HomeComponent } from './components/home/home.component';
+import { MapComponent } from './components/map/map.component';
 
 
 
@@ -14,7 +15,7 @@ export const routes: Routes = [
     {path: '', component: LandingComponent}, //ruta predeterminada 
     {path: 'login', component: LoginComponent},
     {path: 'signup', component: RegisterComponent},
-    {path: 'map', component: HomeMapComponent, canActivate: [AuthGuard] },
+    {path: 'home', component: HomeComponent, canActivate: [AuthGuard] },
     {path: 'profile', component: ProfileComponent, canActivate: [AuthGuard] },
     {path: 'admin', component: AdminComponent, canActivate: [AuthGuard] },
     {path: 'water-form', component: WaterFormComponent, canActivate: [AuthGuard] },
