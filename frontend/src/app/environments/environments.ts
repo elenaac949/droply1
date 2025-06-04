e25c4c656d4ac790c6e968e7fd49b0d5610775b8
elenaac949 <elenaalexandra949@gmail.com>
2025-06-04 23:53:09 +0200

    environmet

--- frontend/src/app/environments/environments.ts
+++ frontend/src/app/environments/environments.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:3000/auth', // Ajusta según tu configuración de Node.js
+  apiUrl: 'http://localhost:3000/', 
 };
---
+++ frontend/src/app/environments/environmet.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: true,
+  apiUrl: 'satisfied-abundance-production.up.railway.app'
+};
